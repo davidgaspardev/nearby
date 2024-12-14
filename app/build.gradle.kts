1fd977e118431a2b53502b068db7742ecc60254c
David Gaspar <davidgaspar.dev@gmail.com>
2024-12-14 15:34:09 -0300

    fix: Update compileSdk from 34 to 35 (to support core-ktx:1.15.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "dev.davidgaspar.nearby"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.davidgaspar.nearby"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/dev/davidgaspar/nearby/ui/component/NearbyButton.kt
@@ -0,0 +1,4 @@
+package dev.davidgaspar.nearby.ui.component
+
+class NearbyButton {
+}
